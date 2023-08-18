7e1ff45e637c26d5f67aa2f5c800ccf3dae69127
nitin931 <nitinthakur931@gmail.com>
2023-08-18 17:30:28 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        B obj = new B();
+        String result = obj.meth();
+        System.out.println(result);
+    }
+
+
 }
